634ebdb5d534ca7c0ab0bcf3cfa3ef2593220f0e
papayoun <pierre.gloaguen@gmail.com>
2021-01-26 17:50:03 +0100

    essai calcul explicites cas gaussien

---
+++ checking_computations.R
@@ -0,0 +1,71 @@
+rm(list = ls())
+# Utils -------------------------------------------------------------------
+
+get_q <- function(x, a, b, x0){
+  dnorm(x, a * x0, b)
+}
+get_g <- function(x, c, d, y){
+  dnorm(y, c * x, d)
+}
+get_a <- function(rho, delta, method = "ou"){
+  if(method == "ou"){
+    return(exp(-rho * delta))
+  }
+  else if(method == "euler"){
+    return(1 - rho * delta)
+  }
+  else{
+    stop("method must be 'ou' or 'euler'")
+  }
+}
+get_b <- function(rho, sigma, delta, method = "ou"){
+  if(method == "ou"){
+    return(sigma * sqrt(0.5 * (1 - exp(-2 * rho * delta)) / rho))
+  }
+  else if(method == "euler"){
+    return(sigma * sqrt(delta))
+  }
+  else{
+    stop("method must be 'ou' or 'euler'")
+  }
+}
+
+get_l <- function(x, a, b, c, d, x0, y){
+  get_q(x, a, b, x0) * get_g(x, c, d, y)
+}
+
+get_gamma2 <- function(b, c, d){
+  gamma2_inv <- c^2 / d^2 + 1 / b^2
+  1 / gamma2_inv
+}
+
+get_integrand <- function(rho, sigma, delta, c, d, x0, y, method = "ou"){
+  a <- get_a(rho, delta, method); 
+  b <- get_b(rho, sigma, delta, method)
+  gamma2 <- get_gamma2(b, c, d)
+  initial_integrand <- sqrt(gamma2) / (b * d * sqrt(2 * pi))
+  eterm <- exp(-0.5 * ((y / d)^2 + (a / b * x0)^2 - gamma2 * (a / b^2 * x0 + c / d^2 * y)^2))
+  initial_integrand * eterm
+}
+
+
+# Example -----------------------------------------------------------------
+
+set.seed(123)
+foo <- function(my_x0, my_y, method){
+  z <- get_integrand(rho = 0.5, sigma = 1, delta = 2 , c = 1, d = 1, my_x0, my_y, method)
+  data.frame(x0 = my_x0, y = my_y, z = z, method = method)
+}
+
+plan_xp <- expand.grid(my_x0 = seq(-4, 4, length.out = 101),
+                       my_y = seq(-4, 4, length.out = 101),
+                       method = c("euler", "ou"))
+library(tidyverse)
+resultats <- purrr::pmap_dfr(plan_xp, foo)
+resultats %>% 
+  group_by(x0, y) %>% 
+  summarise(diff = abs(diff(z))) %>% 
+  ggplot() +
+  aes(x = x0, y = y, fill = diff) +
+  geom_raster() +
+  scale_fill_viridis_c()
